6fe609bd04b7b7df3e0604afe6e2b49c119a687a
Joseph Greene <jgreenemi@gmail.com>
2017-08-05 19:42:06 -0700

    Add publish function.

--- bunalert.py
+++ bunalert.py
@@ -1,10 +1,12 @@
 import boto3
 import logging
+import phonenumbers
 from flask import Flask
 from flask import request
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
-from jinja2 import Template
+from phonenumbers import carrier
+from phonenumbers.phonenumberutil import number_type
 
 app = Flask(__name__)
 
@@ -23,6 +25,7 @@ def index():
 
 
 @app.route('/subscribe', methods=['POST'])
+@app.route('/dev/subscribe', methods=['POST'])
 def subscribe():
     logging.info('subscribe page started.')
     try:
@@ -31,7 +34,11 @@ def subscribe():
         if not subscriber_number:
             raise Exception('No subscriber number was passed in!')
 
-        logging.info('subscribe page got valid subscriber_number.')
+        logging.info('subscribe page checking subscriber_number is valid.')
+        # Checking if subscriber_number matches valid format: +18885554444
+        carrier._is_mobile(number_type(phonenumbers.parse(subscriber_number)))
+
+        logging.info('subscribe page got valid subscriber_number of {}.'.format(subscriber_number))
         # Try to subscribe the number.
         topic_arn = 'arn:aws:sns:us-west-2:277012880214:BunAlert'
         snsclient = boto3.client(
@@ -57,6 +64,47 @@ def subscribe():
         page = 'Something went wrong! Exception: {}'.format(e)
         logging.error(page)
         return page, 400
+
+
+@app.route('/alert', methods=['GET', 'POST'])
+@app.route('/dev/alert', methods=['GET', 'POST'])
+def alert():
+    logging.info('alert page started.')
+    if request.method == 'POST':
+        logger.info('alert page POST requested.')
+        try:
+            form_response = request.form
+            alert_message = form_response['alert_message']
+            if not alert_message:
+                raise Exception('No alert message was passed in!')
+
+            logging.info('alert page got an alert_message!')
+            # Try to publish that message.
+            topic_arn = 'arn:aws:sns:us-west-2:277012880214:BunAlert'
+            snsclient = boto3.client(
+                'sns',
+                region_name='us-west-2'
+            )
+
+            snsclient.publish(Message='Bun Alert Update: A Bun has been spotted! {}'.format(alert_message), TopicArn=topic_arn)
+
+            # If all's well, return to the alert page with success message.
+            template = Environment(loader=FileSystemLoader('html/')).get_template('alert.j2')
+            page = template.render(alert_result='Alert message published successfully!')
+            logging.info('alert page returned.')
+
+        except Exception as e:
+            logging.info('alert page errored.')
+            page = 'Something went wrong! Exception: {}'.format(e)
+            logging.error(page)
+            return page, 400
+    else:
+        # If it's not a POST, must be a GET! Return form page.
+        template = Environment(loader=FileSystemLoader('html/')).get_template('alert.j2')
+        page = template.render()
+        logging.info('alert page returned.')
+        return page, 200
+
 
 if __name__ == '__main__':
     app.run('localhost', '5000')
